14af5bfb8158cbfac0b3925cc11ba95ad05bd52e
myxvisual <myxvisual@live.com>
2019-09-12 10:07:41 +0800

    feat: Update kerer.com

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,7 +1,11 @@
 const fse = require("fs-extra")
 const path = require("path")
 const child_process = require("child_process")
+process.chdir(__dirname)
 
+try {
+    child_process.execSync(`git clone git@github.com:myxvisual/kerer-prod.git ../../kerer-prod`)
+} catch(e) {}
 fse.copySync(path.join(__dirname, "../_book"), path.join(__dirname, "../../kerer-prod"), { overwrite: true })
 
 child_process.execSync(`cd ${path.join(__dirname, "../../kerer-prod")} && git add -A && git commit -m "feat: Update kerer.com" && git push`)
